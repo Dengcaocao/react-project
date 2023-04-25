4fd54adf5dbc8f522e0f03341b3e4bd115e47d8a
dw <1536698087@qq.com>
2023-04-25 16:34:19 +0800

    webox | 引入antd

--- webox-admin/src/App.tsx
+++ webox-admin/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from 'antd'
+import 'antd/dist/reset.css'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      app
+      <Button type="primary">Button</Button>
     </div>
   )
 }
