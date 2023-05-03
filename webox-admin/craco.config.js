9b28efb89199a05078389bc08167b740a4444806
DW <1536698087@qq.com>
2023-05-03 23:25:28 +0800

    webox | 配置webpack

--- webox-admin/craco.config.js
+++ webox-admin/craco.config.js
@@ -1,4 +1,22 @@
 /* craco.config.js */
+const webpack = require('webpack')
 module.exports = {
   // ...
+  webpack: {
+    alias: {},
+    plugins: {
+      add: [
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify('development')
+          }
+        })
+      ],
+      remove: []
+    },
+    configure: {},
+    configure: (webpackConfig, { env, paths }) => {
+      return webpackConfig
+    }
+  }
 }
