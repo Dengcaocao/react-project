170da616636fcbb18e7bc0099857e4744aeb4ac0
dw <1536698087@qq.com>
2023-05-05 16:53:21 +0800

    webox | 登录页

--- webox-admin/src/views/login/login.tsx
+++ webox-admin/src/views/login/login.tsx
@@ -1,21 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import type { CSSProperties } from 'react'
 import {
   AlipayOutlined,
   LockOutlined,
-  MobileOutlined,
   TaobaoOutlined,
   UserOutlined,
   WeiboOutlined
 } from '@ant-design/icons'
 import {
   LoginFormPage,
-  ProFormCaptcha,
-  ProFormCheckbox,
   ProFormText
 } from '@ant-design/pro-components'
-import { Button, Divider, message, Space, Tabs } from 'antd'
-import type { CSSProperties } from 'react'
-import { useState } from 'react'
+import { Divider, Space, Tabs, message } from 'antd'
 
 type LoginType = 'phone' | 'account'
 
@@ -26,38 +23,31 @@ const iconStyles: CSSProperties = {
   cursor: 'pointer'
 }
 
+interface submitParams {
+  username: string,
+  password: string
+}
+
 const Login = () => {
-  const [loginType, setLoginType] = useState<LoginType>('phone')
+  const navigate = useNavigate()
+
+  const [loginType, setLoginType] = useState<LoginType>('account')
+
+  const handleSubmit = async (values: submitParams) => {
+    const { username, password } = values
+    if (username === 'dengcaocao' && password === 'antd') {
+      navigate('/home')
+      message.success('登录成功')
+    }
+  }
+
   return (
-    <div style={{ backgroundColor: 'white', height: 'calc(100vh - 48px)', margin: -24 }}>
+    <div style={{ backgroundColor: 'white', height: '100vh' }}>
       <LoginFormPage
         backgroundImageUrl="https://gw.alipayobjects.com/zos/rmsportal/FfdJeJRQWjEeGTpqgBKj.png"
         logo="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
-        title="Github"
-        subTitle="全球最大的代码托管平台"
-        activityConfig={{
-          style: {
-            boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.2)',
-            color: '#fff',
-            borderRadius: 8,
-            backgroundColor: '#1677FF'
-          },
-          title: '活动标题，可配置图片',
-          subTitle: '活动介绍说明文字',
-          action: (
-            <Button
-              size="large"
-              style={{
-                borderRadius: 20,
-                background: '#fff',
-                color: '#1677FF',
-                width: 120
-              }}
-            >
-              去看看
-            </Button>
-          )
-        }}
+        title="WeBox"
+        subTitle="动画库管理平台"
         actions={
           <div
             style={{
@@ -118,6 +108,7 @@ const Login = () => {
             </Space>
           </div>
         }
+        onFinish={handleSubmit}
       >
         <Tabs
           centered
@@ -125,9 +116,8 @@ const Login = () => {
           onChange={(activeKey) => setLoginType(activeKey as LoginType)}
         >
           <Tabs.TabPane key={'account'} tab={'账号密码登录'} />
-          <Tabs.TabPane key={'phone'} tab={'手机号登录'} />
         </Tabs>
-        {loginType === 'account' && (
+        {loginType === 'account' && 
           <>
             <ProFormText
               name="username"
@@ -158,71 +148,7 @@ const Login = () => {
               ]}
             />
           </>
-        )}
-        {loginType === 'phone' && (
-          <>
-            <ProFormText
-              fieldProps={{
-                size: 'large',
-                prefix: <MobileOutlined className={'prefixIcon'} />
-              }}
-              name="mobile"
-              placeholder={'手机号'}
-              rules={[
-                {
-                  required: true,
-                  message: '请输入手机号！'
-                },
-                {
-                  pattern: /^1\d{10}$/,
-                  message: '手机号格式错误！'
-                }
-              ]}
-            />
-            <ProFormCaptcha
-              fieldProps={{
-                size: 'large',
-                prefix: <LockOutlined className={'prefixIcon'} />
-              }}
-              captchaProps={{
-                size: 'large'
-              }}
-              placeholder={'请输入验证码'}
-              captchaTextRender={(timing, count) => {
-                if (timing) {
-                  return `${count} ${'获取验证码'}`
-                }
-                return '获取验证码'
-              }}
-              name="captcha"
-              rules={[
-                {
-                  required: true,
-                  message: '请输入验证码！'
-                }
-              ]}
-              onGetCaptcha={async () => {
-                message.success('获取验证码成功！验证码为：1234')
-              }}
-            />
-          </>
-        )}
-        <div
-          style={{
-            marginBlockEnd: 24
-          }}
-        >
-          <ProFormCheckbox noStyle name="autoLogin">
-            自动登录
-          </ProFormCheckbox>
-          <a
-            style={{
-              float: 'right'
-            }}
-          >
-            忘记密码
-          </a>
-        </div>
+        }
       </LoginFormPage>
     </div>
   )
