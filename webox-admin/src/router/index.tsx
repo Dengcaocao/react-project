414bd7191cb5029788719c28d7b1297e526600fb
dw <1536698087@qq.com>
2023-05-09 17:26:39 +0800

    webox | banner菜单

--- webox-admin/src/router/index.tsx
+++ webox-admin/src/router/index.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import {
   ChromeFilled,
   CrownFilled,
-  SmileFilled,
-  TabletFilled
+  SmileFilled
 } from '@ant-design/icons'
 import ProLayout from '@/components/ProLayout'
-import banner from '@/views/sandwiches'
+import banner from '@/views/banner'
 import home from '@/views/home/home'
 
 interface IBaseConfig {
@@ -40,30 +39,16 @@ const routes: IRouteConfig[] = [
     ]
   },
   {
-    path: '/admin',
-    name: '管理页',
+    path: '/manage',
+    name: '数据管理',
     icon: <CrownFilled />,
     access: 'canAdmin',
     component: ProLayout,
     routes: [
       {
-        path: '/admin/sub-page1',
-        name: '一级页面',
+        path: '/manage/banner',
+        name: 'banner数据',
         icon: 'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
-        component: banner
-      }
-    ]
-  },
-  {
-    name: '列表页',
-    icon: <TabletFilled />,
-    path: '/list',
-    component: ProLayout,
-    routes: [
-      {
-        path: '/list/sub-page',
-        name: '列表页面',
-        icon: <CrownFilled />,
         component: banner
       }
     ]
