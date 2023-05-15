1d7e0f92410da79bd2d0e171725e013e84cc35e3
dw <1536698087@qq.com>
2023-05-15 15:02:11 +0800

    webox | 添加sessionStorage工具

--- webox-admin/src/utils/util.ts
+++ webox-admin/src/utils/util.ts
@@ -1,6 +1,17 @@
+export type keyType = 'webox-banner' | 'webox-recommend' | 'webox-demo'
+
+export const setSessionStorage = (key: keyType, val: string) => {
+  sessionStorage.setItem(key, val)
+}
+
+export const getSessionStorage = (key: keyType) => {
+  return sessionStorage.getItem(key)
+}
+
 export const waitTime = (time: number) => {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve(true)
     }, time)
   })
+}
