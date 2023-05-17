350458564262cbc63a3015f70b97f22d3877b212
DW <1536698087@qq.com>
2023-05-17 21:35:06 +0800

    webox | 瀑布流组件

--- webox-admin/src/components/Waterfall/Waterfall.tsx
+++ webox-admin/src/components/Waterfall/Waterfall.tsx
@@ -1,35 +1,35 @@
 import React, { useEffect, useState } from 'react'
-import { useAppSelector } from '@/store/hook'
 
 interface propsType {
+  width?: number,
+  gap?: number,
+  transition?: number,
+  watch?: any[],
   children: React.ReactElement[]
 }
 
 const Waterfall = (props: propsType) => {
   
-  const { children } = props
+  const { width, gap, transition, watch, children } = props
 
-  const collapsed = useAppSelector(state => state.appReducer.collapsed)
+  const [itemWidth] = useState<number>(width || 200) // setItemWidth
 
-  const [itemWidth] = useState<number>(200) // setItemWidth
-
-  const handleImgLoad = async () => {
-    const promiseArr: any[] = []
-    const imgs: NodeListOf<HTMLImageElement> = document.querySelectorAll('.waterfall img')
-    imgs.forEach(img => {
+  // 加载图片
+  const handleImgLoad = () => {
+    const promiseArr: Promise<unknown>[] = []
+    const imgtags: NodeListOf<HTMLImageElement> = document.querySelectorAll('.waterfall img')
+    imgtags.forEach(img => {
       const p = new Promise(resolve => {
         img.onload = () => resolve(true)
       })
       promiseArr.push(p)
     })
-    return await Promise.all(promiseArr)
   }
 
   const handleArrange = async () => {
     const waterfallNode = document.querySelector('.waterfall') as HTMLElement
     // 容器宽度
     const cWidth = document.querySelector('.container')?.clientWidth
-    console.log(cWidth)
     // 列数量
     const colCount = Math.floor(cWidth as number / itemWidth)
     // 存储每列的高度
@@ -38,15 +38,14 @@ const Waterfall = (props: propsType) => {
     let minIndex = 0
     // 容器下的所有子节点
     const nodeList: NodeListOf<ChildNode> = waterfallNode.childNodes
-    // 等待图片加载完成
-    await handleImgLoad()
+    // 定位
     nodeList.forEach((node: any) => {
-      console.log(node)
       node.style.position = 'absolute'
-      node.style.padding = '10px'
+      node.style.padding = `${gap || 10}px`
       node.style.width = itemWidth + 'px'
       node.style.top = `${colsHeight[minIndex]}px`
-      node.style.left = `${minIndex * 200}px`
+      node.style.left = `${minIndex * itemWidth}px`
+      node.style.transition = `${transition || 0.5}s`
       const itemHeight = node.clientHeight
       // 设置列的高度和数据
       colsHeight[minIndex] += itemHeight
@@ -54,15 +53,20 @@ const Waterfall = (props: propsType) => {
       minIndex = colsHeight.indexOf(Math.min(...colsHeight))
     })
     if (waterfallNode) {
-      waterfallNode.style.width = colCount * 200 + 'px'
+      waterfallNode.style.width = colCount * itemWidth + 'px'
       waterfallNode.style.height = Math.max(...colsHeight) + 'px'
     }
   }
 
   useEffect(() => {
-    // setTimeout(handleArrange, 200)
-    handleArrange()
-  }, [collapsed])
+    handleImgLoad()
+  }, [])
+
+  useEffect(() => {
+    window.onresize = handleArrange
+    // 延迟执行获取更新后容器的宽度
+    setTimeout(handleArrange, 150)
+  }, watch || [])
 
   return (
     <div className='container'>
